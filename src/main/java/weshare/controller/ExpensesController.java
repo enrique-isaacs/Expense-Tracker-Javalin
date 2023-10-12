09f8d17aaa77e023bc5c945fc31f16db6f06e5c3
enisaac022 <enisaac022@student.wethinkcode.co.za>
2023-10-12 16:43:48 +0200

    Added link to expenses page to make a payment request for a specific expense

--- src/main/java/weshare/controller/ExpensesController.java
+++ src/main/java/weshare/controller/ExpensesController.java
@@ -4,12 +4,18 @@ import io.javalin.http.Handler;
 import org.javamoney.moneta.function.MonetaryFunctions;
 import org.jetbrains.annotations.NotNull;
 import weshare.model.Expense;
+import weshare.model.MoneyHelper;
 import weshare.model.Person;
 import weshare.persistence.ExpenseDAO;
+import weshare.server.Routes;
 import weshare.server.ServiceRegistry;
 import weshare.server.WeShareServer;
 
 import javax.money.MonetaryAmount;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Collection;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -23,7 +29,43 @@ public class ExpensesController {
         Person personLoggedIn = WeShareServer.getPersonLoggedIn(context);
 
         Collection<Expense> expenses = expensesDAO.findExpensesForPerson(personLoggedIn);
-        Map<String, Object> viewModel = Map.of("expenses", expenses);
+        MonetaryAmount totalExpense = totalExpense(expenses);
+        Map<String, Object> viewModel = Map.of("expenses", expenses, "totalExpense", totalExpense);
         context.render("expenses.html", viewModel);
     };
+
+    private static MonetaryAmount totalExpense(Collection<Expense> expenses) {
+        MonetaryAmount total = MoneyHelper.amountOf(0);
+        for (Expense expense : expenses) {
+            total = total.add(expense.getAmount());
+        }
+        return total;
+    }
+
+    public static final Handler newexpense = ctx -> {
+        
+        ctx.render("newexpense.html");
+        
+    };
+
+    public static final Handler expenseAction = ctx -> {
+        ExpenseDAO expensesDAO = ServiceRegistry.lookup(ExpenseDAO.class);
+        Person personLoggedIn = WeShareServer.getPersonLoggedIn(ctx);
+
+        // Extracting form data
+        String description = ctx.formParamMap().get("description").get(0);
+        MonetaryAmount amount = MoneyHelper.amountOf(Integer.parseInt(ctx.formParamMap().get("amount").get(0)));
+        String dateStr = ctx.formParamMap().get("date").get(0);
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate date = LocalDate.parse(dateStr, dateFormatter);
+
+        // Create expense
+        Expense expense = new Expense(personLoggedIn, description, amount, date);
+        expensesDAO.save(expense);
+        
+
+        ctx.redirect(Routes.EXPENSES);
+    };
+
+
 }
--- src/main/java/weshare/server/Routes.java
+++ src/main/java/weshare/server/Routes.java
@@ -10,8 +10,10 @@ public class Routes {
     public static final String LOGIN_ACTION = "/login.action";
     public static final String LOGOUT = "/logout";
     public static final String EXPENSES = "/expenses";
+    public static final String NEWEXPENSES = "/newexpenses";
     public static final String PAYMENTREQUESTSENT = "/paymentrequests_sent";
     public static final String PAYMENTREQUESTRECEIVED = "/paymentrequests_received";
+    public static final String CREATEEXPENSE = "/expense.action";
 
     public static void configure(WeShareServer server) {
         server.routes(() -> {
@@ -20,6 +22,8 @@ public class Routes {
             get(EXPENSES,           ExpensesController.view);
             get(PAYMENTREQUESTSENT, PaymentRequestsController.paymentRequestSentView);
             get(PAYMENTREQUESTRECEIVED, PaymentRequestsController.paymentRequestReceivedView);
+            get(NEWEXPENSES,           ExpensesController.newexpense);
+            post(CREATEEXPENSE, ExpensesController.expenseAction);
         });
     }
 }
